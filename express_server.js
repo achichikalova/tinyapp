bf448d12f54e2931c4a6e7553f83bcf1413efc76
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-09-28 18:48:48 +0000

    WIP: install nodemon, add Delete

--- express_server.js
+++ express_server.js
@@ -37,18 +37,21 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+app.post("/urls", (req, res) => {  
   const shortURL = generateRandomString();
-  const templateVars = { shortURL: shortURL, longURL: req.body.longURL};
   urlDatabase[shortURL] = req.body.longURL;
-  res.render("urls_show", templateVars);
+  res.redirect("urls");
 });
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
