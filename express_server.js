714a0ff83ee611f76445064a99a8a9954dc0f6d3
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-10-01 02:25:13 +0000

    Add README file

--- express_server.js
+++ express_server.js
@@ -1,32 +1,17 @@
-const { findUserByEmail, createUser, authenticateUser } = require('./helpers');
+const { findUserByEmail, createUser, authenticateUser, extractUrlsForUser } = require('./helpers');
 const express = require('express');
 const app = express(); // creating an Express app
-const path = require('path');
-const cookieParser = require("cookie-parser");
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const PORT = 8080;
 
-app.use(cookieParser());
 app.use(cookieSession({
   name: 'session',
   keys: ['hello from the other side', 'cool thing we are doing']
 }));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs'); // Setting ejs as the template engine
-
-app.get('/', (req, res) => {
-  const userId = req.session.user_id;
-  const loggedInUser = users[userId];
-	const allowedPaths = ['/', '/login'];
-
-	if (!loggedInUser && allowedPaths.includes(path)) {
-		res.redirect('/login');
-	} else {
-    res.redirect('/urls');
-  }
-});
 
 const generateRandomString = () => {
   return Math.random().toString(36).substr(2, 6);
@@ -51,8 +36,10 @@ const urlDatabase = {
     userID: 'dhd5hg'
   }
 };
+//Hashed password for exist users
 const hashedPassword1 = bcrypt.hashSync('purple-monkey-dinosaur', 10);
 const hashedPassword2 = bcrypt.hashSync('dishwasher-funk', 10);
+
 const users = {
   'fgdr5n': {
     id: 'fgdr5n',
@@ -67,7 +54,7 @@ const users = {
 };
 
 app.get('/register', (req, res) => {
-  const userId = req.session.user_id;
+  const userId = req.session.userId;
   const loggedInUser = users[userId];
   const templateVars = { user: loggedInUser };
   res.render('urls_register', templateVars);
@@ -84,9 +71,8 @@ app.post('/register', (req, res) => {
     return;
   }
   const userId = createUser(email, password, users); //use helper function to create a new user in usersDb and get the id
-  req.session.user_id = userId;
+  req.session.userId = userId;
   res.redirect('/urls');
-
 });
 
 app.get('/login', (req, res) => {
@@ -96,65 +82,51 @@ app.get('/login', (req, res) => {
 
 app.post('/login', (req, res) => {
   const {email, password} = req.body;
-  const user = authenticateUser(email, password, users);
+  const user = authenticateUser(email, password, users); //use helper function to check users credentials
   if (!user) {
     return res.status(403).send('Wrong credentials!');
   }
-  req.session.user_id = user.id;
+  req.session.userId = user.id;
   return res.redirect('urls');
 });
 
 app.post('/logout', (req, res) => {
-  req.session = null;
+  req.session = null; //clear cookies
   res.redirect('/urls');
 });
 
-const extractUrlsForUser = (db, id) => {
-  let new_db = {};
-  for (let key in db) {
-    if (db[key].userID === id) {
-      new_db[key] = {
-        longURL: db[key].longURL
-      };
-    }
-  }
-  return new_db;
-}
-
 app.get('/urls', (req, res) => {
-  const userId = req.session.user_id;
+  const userId = req.session.userId;
   const loggedInUser = users[userId];
-  const user_urls = extractUrlsForUser(urlDatabase, userId);
-  const templateVars = { urls: user_urls, user: loggedInUser, id: userId };
+  const userUrls = extractUrlsForUser(urlDatabase, userId); //use helper function to retrieve data for current user
+  const templateVars = { urls: userUrls, user: loggedInUser, id: userId };
   res.render('urls_index', templateVars);
 });
 
 app.post('/urls', (req, res) => {
   const shortURL = generateRandomString();
-  const userId = req.session.user_id; 
+  const userId = req.session.userId;
   const longURL = req.body.longURL;
   urlDatabase[shortURL] = { longURL: longURL, userID: userId };
   res.redirect('/urls');
 });
 
 app.get('/urls/new', (req, res) => {
-  const userId = req.session.user_id;
+  const userId = req.session.userId;
   const loggedInUser = users[userId];
   const templateVars = { user: loggedInUser };
   if (!loggedInUser) {
-    res.redirect('/login');
-    return;
+    return res.redirect('/login');
   }
-  res.render('urls_new', templateVars);
+  return res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:id', (req, res) => {
-  const userId = req.session.user_id;
+  const userId = req.session.userId;
   const loggedInUser = users[userId];
   const templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id], user: loggedInUser };
   if (!loggedInUser) {
-    res.redirect('/login');
-    return;
+    return res.redirect('/login');
   }
   return res.render('urls_show', templateVars);
 });
@@ -173,7 +145,7 @@ app.get('/u/:id', (req, res) => {
 });
 
 app.post('/urls/:id/delete', (req, res) => {
-  const userId = req.session.user_id;
+  const userId = req.session.userId;
   const loggedInUser = users[userId];
   const shortURL = req.params.id;
   if (!loggedInUser) {
@@ -184,5 +156,5 @@ app.post('/urls/:id/delete', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Tinyapp listening on port ${PORT}!`);
+  console.log(`Tiny app listening on port ${PORT}!`);
 });
--- helpers.js
+++ helpers.js
@@ -1,9 +1,10 @@
+const bcrypt = require('bcryptjs');
+
 const generateRandomString = () => {
+  //Helper Functions
   return Math.random().toString(36).substr(2, 6);
 };
 
-const bcrypt = require('bcryptjs');
-//Helper Functions
 const findUserByEmail = (email, usersDb) => { //Check if user already exists
   for (let userId in usersDb) {
     const user = usersDb[userId];
@@ -26,13 +27,22 @@ const createUser = (email, password, usersDb) => {  // Create a new user
 
 const authenticateUser = (email, password, usersDb) => { // Check and retrieve the user from the userDb
   const userFound = findUserByEmail(email, usersDb);
-  if (userFound) {
-  } if (bcrypt.compareSync(password, userFound.password)) { // Check if password is correct
-    return userFound; // Return user if password is correct    
+  if (userFound && bcrypt.compareSync(password, userFound.password)) { // Check if password is correct
+    return userFound; // Return user if password is correct
   }
   return false; // Return false if password is not correct
 };
 
-//const extractUrlsForUser = (urlsDB, userID) {};
+const extractUrlsForUser = (db, id) => {
+  let newDb = {};
+  for (let key in db) {
+    if (db[key].userID === id) {
+      newDb[key] = {
+        longURL: db[key].longURL
+      };
+    }
+  }
+  return newDb;
+};
 
-module.exports = { findUserByEmail, createUser, authenticateUser };
+module.exports = { findUserByEmail, createUser, authenticateUser, extractUrlsForUser };
